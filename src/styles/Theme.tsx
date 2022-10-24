4c467fc0d1f0a3241b8313b5b6a7225690b2d9cb
choyeon <rlachdus2@naver.com>
2022-10-24 21:54:43 +0900

    chore: add Global Style & icons & fonts

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,41 @@
+const colors = {
+  primary: '#FFCB7C',
+  secondary1: '#BBB7B0',
+  secondary2: '#BFDAF3',
+  black: '#342E21',
+  white: '#FFFFFF',
+  grey1: '#9A958B',
+  grey2: '#BBB7B0',
+  grey3: '#CFCCC4',
+  grey4: '#E5E4E0',
+  grey5: '#F2F2F0',
+  warning: '#DE5C5C',
+  notice1: '#F98A67',
+  notice2: '#94CD8A',
+};
+
+const fonts = {
+  h1Bold: 'font-size: 1.25rem; font-weight:700; line-height: 1.6rem',
+  h1Regular: 'font-size: 1.25rem; font-weight:400; line-height: 1.6rem',
+  h1Light: 'font-size: 1.25rem; font-weight:300; line-height: 1.6rem',
+  h2Bold: 'font-size: 1.125rem; font-weight:700; line-height: 1.5rem',
+  h2Regular: 'font-size: 1.125rem; font-weight:400; line-height: 1.5rem',
+  h2Light: 'font-size: 1.125rem; font-weight:300; line-height: 1.5rem',
+  body1Bold: 'font-size: 1rem; font-weight:700; line-height: 1.4rem',
+  body1Regular: 'font-size: 1rem; font-weight:400; line-height: 1.4rem',
+  body1Light: 'font-size: 1rem; font-weight:300; line-height: 1.4rem',
+  body2Bold: 'font-size: 0.875rem; font-weight:700; line-height: 1.25rem',
+  body2Regular: 'font-size: 0.875rem; font-weight:400; line-height: 1.25rem',
+  body2Light: 'font-size: 0.875rem; font-weight:300; line-height: 1.25rem',
+  badge1Bold: 'font-size: 0.8rem; font-weight:700; line-height: 1.4rem',
+  badge1Regular: 'font-size: 0.8rem; font-weight:400; line-height: 1.4rem',
+  badge2Bold: 'font-size: 0.75rem; font-weight:700; line-height: 1.25rem',
+  badge2Regular: 'font-size: 0.75rem; font-weight:400; line-height: 1.25rem',
+};
+
+const DefaultTheme = {
+  colors,
+  fonts,
+};
+
+export default DefaultTheme;
