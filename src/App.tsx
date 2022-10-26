a2d2c2d926bd0b305b75098dd17188eadb14b9b9
choyeon <rlachdus2@naver.com>
2022-10-26 13:47:53 +0900

    #3 feat: add 상세페이지 도서 정보 UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import './App.css';
+import Router from './router/Router';
+import { ThemeProvider } from 'styled-components';
+import DefaultTheme from './styles/Theme';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return <div className="App" />;
