cc19c1d74b14039611abcc012c95e70c07e05199
YoujungSon <andn1026@gmail.com>
2022-11-01 23:15:20 +0900

    #12 feat: category UI 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import { ThemeProvider } from 'styled-components';
 import DefaultTheme from './styles/Theme';
 import GlobalStyle from './styles/GlobalStyle';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <RecoilRoot>
-      <ThemeProvider theme={DefaultTheme}>
-        <GlobalStyle />
-        <Router />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={DefaultTheme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
