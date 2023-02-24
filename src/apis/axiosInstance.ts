5535b0bc63f6ce90065869fb0d1f4795f21141cf
vnfdusdl <rlachdus2@naver.com>
2023-02-24 15:35:42 +0900

    #44 feat: boardAxiosInstance에도 access token 재발급 로직 추가

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -37,3 +37,27 @@ authAxiosInstance.interceptors.response.use(
   },
 );
 
+boardAxiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      console.log('401')
+      try {
+        originalRequest._retry = true;
+        const { id, accessToken, refreshToken, nickname } = await refreshAccessToken();
+        console.log(id, accessToken, refreshToken, nickname);
+        window.localStorage.setItem('user', JSON.stringify({ id, accessToken, refreshToken, nickname }));
+        authAxiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+        return authAxiosInstance(originalRequest);
+      } catch (error) {
+        localStorage.clear();
+        window.location.href = '/';
+        alert('다시 로그인 해주세요.');
+      }
+    }
+    return Promise.reject(error);
+  },
+);
