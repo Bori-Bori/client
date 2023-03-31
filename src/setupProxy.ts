70eccef16dac5d2f239a57d501792a87a780ec45
YoujungSon <andn1026@gmail.com>
2023-03-31 21:15:30 +0900

    feat: proxy 설정 추가

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,14 +1,24 @@
+import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
-import { Application } from 'express';
+import cors from 'cors';
 
-export default function configureProxy(app: Application) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://www.aladin.co.kr/ttb',
-      pathRewrite: {
-        '^/api': '',
-      },
-    }),
-  );
-}
+const app = express();
+
+// CORS 설정
+app.use(cors());
+
+// 프록시 설정
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'http://www.aladin.co.kr/ttb',
+    pathRewrite: {
+      '^/api': '',
+    },
+  }),
+);
+
+// 서버 실행
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
