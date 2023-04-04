82a0bd2d8be9b9d859611bacee5c4f1389121a95
YoujungSon <andn1026@gmail.com>
2023-04-04 10:32:43 +0900

    feat: boardAxiosInstance 삭제

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-export const boardAxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BOARD_BASE_URL,
-});
 export const aladinAxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_ALADIN_URL,
 });
